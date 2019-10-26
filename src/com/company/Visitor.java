8ae05ff0961576e44738f63dc510bbd471737fcc
Goldy <vikk@live.se>
2019-10-26 15:31:29 +0200

    Method for entrance fee

--- src/com/company/Visitor.java
+++ src/com/company/Visitor.java
@@ -4,4 +4,14 @@ public class Visitor extends Person{
     public Visitor(int age, String name) {
         super(age, name);
     }
+
+    public float getEntranceFee(){
+        int age = getAge();
+        if (age <= 8)
+            return 0f;
+        else if (age <= 18)
+            return 3.60f;
+        else
+            return 8.9f;
+    }
 }
